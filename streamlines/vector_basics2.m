240a82eec3ba4ad6dab9d61b8348dad40780cfbc
Bob Weigel <rweigel@gmu.edu>
2014-11-14 12:47:48 -0500

    misc

--- streamlines/vector_basics2.m
+++ streamlines/vector_basics2.m
@@ -24,7 +24,7 @@ for i = 1:10
         plot(j,i,'b.','MarkerSize',20);
     end
 end
-text(1,0.2,'Measurements of velocity (v_x,v_y)','FontSize',16);
+text(3,0.2,'Measurements of velocity (v_x,v_y)','FontSize',16);
 ylabel(gca, 'y','Rotation',0,'FontSize',16);
 xlabel(gca,'x','FontSize',16);
 axis square
@@ -35,3 +35,33 @@ set(gca,'FontSize',16)
 
 print -depsc vector_basics2quiver.eps
 print -dpng vector_basics2quiver.png
+
+px(1) = 1.0;
+py(1) = 1.0;
+vx(1) = interp2(X,Y,U,px(1),py(1));
+vy(1) = interp2(X,Y,V,px(1),py(1));
+
+h = 0.1;
+Nsteps = 100;
+for t = 2:Nsteps
+    px(t) = px(t-1) + h*vx(t-1);
+    py(t) = py(t-1) + h*vy(t-1);
+    
+    if (px(t) > 10.0 || py(t) > 10.0 || px(t) < 1.0 || py(t) < 1.0)
+        % If next position is out-side of grid, stop computing positions.
+        break;
+    end
+   
+    vx(t) = interp2(X,Y,U,px(t),py(t));
+    vy(t) = interp2(X,Y,V,px(t),py(t));
+    fprintf('px(%d) = %.2f  py(%d) = %.2f  vx(%d) = %.2f  vy(%d) = %.2f\n',...
+            t,px(t),t,py(t),t,vx(t),t,vy(t));
+    
+end
+
+plot(px,py,'g','LineWidth',3);
+plot(px,py,'k.','MarkerSize',10);
+plot(px(1),py(1),'kx','MarkerSize',15);
+
+print -depsc vector_basics2contour.eps
+print -dpng vector_basics2contour.png
--- streamlines/vector_basics4.m
+++
@@ -1,8 +0,0 @@
-clear
-[X,Y] = meshgrid([-pi:0.1:pi],[-2:0.1:2]);
-U = Y;
-V = -sin(X);
-
-quiver(X,Y,U,V)
-
-
