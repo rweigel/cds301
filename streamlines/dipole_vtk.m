58bdfb79c323896c8c6380028dc7ae15f2963a4f
Bob Weigel <rweigel@gmu.edu>
2014-11-25 15:57:28 -0500

    misc

---
+++ streamlines/dipole_vtk.m
@@ -0,0 +1,37 @@
+clear
+fid = fopen('dipole_vtk.vtk','w');
+fprintf(fid,'# vtk DataFile Version 3.0\n');
+fprintf(fid,'Dipole Field\n');
+fprintf(fid,'ASCII\n');
+
+x = [-10:1:10];
+y = [-10:1:10];
+z = [-10:1:10];
+
+fprintf(fid,'DATASET STRUCTURED_POINTS\n');
+fprintf(fid,'DIMENSIONS %d %d %d\n',length(x),length(y),length(z));
+fprintf(fid,'ORIGIN %f %f %f\n',x(1),y(1),z(1));
+fprintf(fid,'SPACING 1.0 1.0 1.0\n');
+
+fprintf(fid,'POINT_DATA %d\n',length(x)*length(y)*length(z));
+fprintf(fid,'VECTORS MagneticField float\n');
+
+
+
+l = 1;
+for k = 1:length(z)
+    for j = 1:length(y)
+        for i = 1:length(x)
+            p(l,:) = [x(i),y(j),z(k)];
+            l = l+1;
+        end
+    end
+end
+
+b = B_dipole(p);
+
+b(isnan(b)) = 0;
+fprintf(fid,'%e %e %e\n',b');
+fclose(fid);
+type dipole_vtk.vtk
+
--- streamlines/vector_basics2.m
+++ streamlines/vector_basics2.m
@@ -1,37 +1,67 @@
-clear;
-for i = 1:10
-    for j = 1:10
-        X(i,j) = j;
-        Y(i,j) = i;
-        U(i,j) = 1.0;
-        V(i,j) = 1.0+j/2;
+clear; clc;
+
+x = [0:20:100];
+y = [0:20:100];
+
+
+for i = 1:length(x)
+    for j = 1:length(y)
+        z(i,j) = (x(i)-50)^2 + (y(j)-50)^2;
     end
 end
 
-% or
-% [X,Y] = meshgrid([1:10],[1:10]);
-% U = ones(size(X));
-% V = 1+X/2;
+z = z/1000;
+figure(1);clf;
+axis([-20 120 -20 120]); % Set axis wider than default
+hold on;
 
-figure(1);clf;hold on;
-axis([0 11 0 11]); % Set axis wider than default
-quiver(X,Y,U,V);
+for i = 1:length(x)
+    for j = 1:length(y)
+        plot(x(i),y(j),'k.','MarkerSize',20);
+        text(2+x(i),2+y(j),num2str(z(i,j)));
+    end
+end
+
+axis square;
+xlabel('x');
+ylabel('y');
+title('Values of z=0.1[(x-50)^2+(y-50)^2]')
+
+%figure(2);clf;clc;
+%axis([-20 120 -20 120]);
 
 hold on;
-for i = 1:10
-    for j = 1:10
-        text(j-0.5,i-0.2,[sprintf('(%.1f,%.1f)',U(i,j),V(i,j))],'Color','black','FontSize',8);
-        plot(j,i,'b.','MarkerSize',20);
+
+for i = 2:length(x)-1
+    for j = 2:length(y)-1
+        if (z(i,j) == 1)
+            fprintf('z(%d,%d) = 1 (x=%d,y=%d)\n',i,j,x(i),y(j));
+                
+            if (z(i-1,j) == 1) % Left
+                fprintf('   Left: z(%d,%d) = 1 (x=%d,y=%d)\n',i-1,j,x(i-1),y(j));
+                plot([x(i),x(i-1)],[y(j),y(j)]); % Draw line that connects.
+            end
+            if (z(i+1,j) == 1) % Right
+                fprintf('   Right: z(%d,%d) = 1 (x=%d,y=%d)\n',i+1,j,x(i+1),y(j));
+            end
+            if (z(i,j+1) == 1) % Above
+                fprintf('   Above: z(%d,%d) = 1 (x=%d,y=%d)\n',i+1,j,x(i),y(j+1));
+                plot([x(i),x(i)],[y(j),y(j+1)]); % Draw line that connects.
+            end
+            if (z(i,j-1) == 1) % Below
+                fprintf('   Below: z(%d,%d) = 1 (x=%d,y=%d)\n',i+1,j,x(i),y(j-1));
+            end
+            if (z(i-1,j-1) == 1) % Up 1, Right 1
+               plot([x(i),x(i-1)],[y(j),y(j-1)]); % Draw line that connects.
+            end
+            if (z(i-1,j+1) == 1) % Up 1, Left 1
+               plot([x(i),x(i-1)],[y(j),y(j+1)]); % Draw line that connects.
+            end
+        end
     end
 end
-text(1,0.2,'Measurements of velocity (v_x,v_y)','FontSize',16);
-ylabel(gca, 'y','Rotation',0,'FontSize',16);
-xlabel(gca,'x','FontSize',16);
-axis square
-set(gca,'XTick',[1:1:10])
-set(gca,'YTick',[1:1:10])
-grid on;
-set(gca,'FontSize',16)
 
-print -depsc vector_basics2_quiver.eps
-print -dpng vector_basics2_quiver.png
+axis square;
+xlabel('x');
+ylabel('y');
+title('Values of z=0.1[(x-50)^2+(y-50)^2]')
