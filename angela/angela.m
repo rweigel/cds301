c26c7891f510035aff7703e69e44cc512cceb0f0
Bob Weigel <rweigel@gmu.edu>
2014-12-05 12:23:42 -0500

    Code for angela

---
+++ angela/angela.m
@@ -0,0 +1,73 @@
+clear;
+fid = fopen('a.csv');
+i = 1;
+line = fgetl(fid);
+line = fgetl(fid);
+line = fgetl(fid);
+
+while (1)
+    line = fgetl(fid);
+    if ~ischar(line)
+        break;
+    end
+    fprintf('%02d Original: %s\n',i,line);
+    line = regexprep(line,'NONE','0');
+    line = regexprep(line,'none','0');
+
+    line = regexprep(line,'SA','0');
+
+    line = regexprep(line,'VH','1');
+    line = regexprep(line,'RS','2');
+    line = regexprep(line,'GF','3');
+    line = regexprep(line,'IN','4');
+    line = regexprep(line,'NA','5');
+
+    line = regexprep(line,'A','1','ignorecase');
+    line = regexprep(line,'B','2','ignorecase');
+    line = regexprep(line,'C','3','ignorecase');
+    line = regexprep(line,'D','4','ignorecase');
+    line = regexprep(line,'E','5','ignorecase');
+    line = regexprep(line,'F','6','ignorecase');
+    line = regexprep(line,'W','7','ignorecase');
+
+    line = regexprep(line,'-,',',');
+    line = regexprep(line,'+,',',');
+
+    line = regexprep(line,'^,','-1,');
+    line = regexprep(line,',$',',-1');
+
+    fprintf('%02d Modified: %s\n',i,line);
+
+    if (1)
+        line = regexprep(line,',,',',-1,');
+        fprintf('%02d Modified: %s\n',i,line);
+        line = regexprep(line,',,',',-1,');
+        fprintf('%02d Modified: %s\n',i,line);
+    end
+    b(i,:) = str2num(line);
+    fprintf('%02d Length: %d\n',i,length(b));
+    fprintf('_____________________________________\n');
+    
+    i = i+1;
+
+end
+fclose(fid);
+b(:,end) = b(:,end)*100; % Convert GPA to integer.
+fprintf('G#   SECT   ENT   Q26   Q27   Q28   Q29   Q30   EXIT   Q26   Q27   Q28   Q29   Q30   GRADE   INST   GP\n');
+for i = 1:size(b,1)
+    fprintf('%d   ',b(i,:)');
+    fprintf('\n');
+end
+
+I = find(b(:,2) == 206);
+b(I,17)
+
+I = find(b(:,3) > -1 & b(:,9) > -1); % Remove empties
+plot(b(I,3),b(I,9),'.','MarkerSize',30);
+xlabel('Entrance Score');
+ylabel('Exit Score');
+tmp = corrcoef(b(:,3),b(:,9));
+title(sprintf('cc = %.2f',tmp(2)));
+grid on;
+
+
